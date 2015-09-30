068a874b33a62373a973d38eb50ef686fc91eea6
zhushitong <zhushitong@intern.douban.com>
2015-10-01 01:10:57 +0800

    Site updated: 2015-10-01 01:10:57

---
+++ js/abstract_trigger.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("#btn_ieee_ntk").click(function(){$("#abs_ieee_ntk").toggle();});$("#btn_sac_15").click(function(){$("#abs_sac_15").toggle();});$("#btn_racs_15").click(function(){$("#abs_racs_15").toggle();});$("#btn_jsw_14").click(function(){$("#abs_jsw_14").toggle();});$("#btn_jcp_14").click(function(){$("#abs_jcp_14").toggle();});});
